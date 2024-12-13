26810121e13754a841c8e4ba591a7e8219696b7d
GoodKn1ght <oleh.sokal.shi.2023@lpnu.ua>
2024-12-13 13:41:28 +0200

    add appoinsights

--- BANK/deployment.py
+++ BANK/deployment.py
@@ -1,11 +1,21 @@
 import os
 
+from bokeh.colors.named import azure
+from django.db import connection
+
 from .settings import *
 from .settings import BASE_DIR
+from azure.monitor.opentelemetry import configure_azure_monitor
+
+
+os.environ["APPLICATIONINSIGHTS_CONNECTION_STRING"] = "InstrumentationKey=2c52f5a4-668a-43da-b21c-dab5c8c49ccc;IngestionEndpoint=https://polandcentral-0.in.applicationinsights.azure.com/;LiveEndpoint=https://polandcentral.livediagnostics.monitor.azure.com/;ApplicationId=c562c434-d5bb-4ea9-8070-46b0abc01ebb"
+configure_azure_monitor()
 
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
 DEBUG = False
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
